af620784ae4ed22f8212a06db7c1cdaf271d11f3
Marco Faustinelli <contatti@faustinelli.net>
2012-12-20 14:23:17 +0100

    initiated chapter 14 - leftmost plain version

--- seasonedFunkyJavascripter_0.1.js
+++ seasonedFunkyJavascripter_0.1.js
@@ -1,5 +1,6 @@
 /* a few implementations from the Seasoned Schemer
 */
+// chapter 11
 function scramble(aTup) {  // tuple = list of numbers
 	function scramble_b(tup, revpre) {
 		if (isEmpty(tup)) return EMPTY;
@@ -10,7 +11,26 @@ function scramble(aTup) {  // tuple = list of numbers
 	}
 	return scramble_b(aTup,EMPTY)
 }
+// chapter 12
+// union con letrec === innerfunction
 
+
+// chapter 14
+// leftmost (first version)
+function leftmostFirstVersion(sexp){
+	if (isEmpty(sexp)) return EMPTY
+	else if (isAtom(car(sexp))) return car(sexp)
+	else {
+		if (!isEmpty(leftmostFirstVersion(car(sexp)))) 
+			return leftmostFirstVersion(car(sexp))
+		else return leftmostFirstVersion(cdr(sexp))
+	}
+}
+
+// depth + let
+
+
+// leftmost + letcc
 
 
 
--- sfjTests.js
+++ sfjTests.js
@@ -5,7 +5,7 @@ YAHOO.namespace('SFJ.test');
 
 var Assert = YAHOO.util.Assert;
 
-// chapter 1
+// chapter 11
 YAHOO.SFJ.test.oTestScramble = new YAHOO.tool.TestCase({
 	name : "TestScramble",
 	testScramble : function() {
@@ -14,15 +14,26 @@ YAHOO.SFJ.test.oTestScramble = new YAHOO.tool.TestCase({
 	}
 });
 
+// chapter 14
+YAHOO.SFJ.test.oTestLeftmostFirst = new YAHOO.tool.TestCase({
+	name : "TestLeftmostFirst",
+	testLeftmostFirst : function() {
+		var input = ArrayToList([[[],1],[[],[1,2]],2,3,2])
+		Assert.areEqual(1,leftmostFirstVersion(input))
+	}
+});
+
 
 YAHOO.util.Event
 		.onDOMReady(function() {
-		
+
 			if (!YAHOO.SFJ.test.SFJ_TestSuite) 
 				YAHOO.SFJ.test.SFJ_TestSuite = new YAHOO.tool.TestSuite("YUI Test Suite for Seasoned Funky Javascripter");
-		
+
 			YAHOO.SFJ.test.SFJ_TestSuite
 					.add(YAHOO.SFJ.test.oTestScramble);
+			YAHOO.SFJ.test.SFJ_TestSuite
+					.add(YAHOO.SFJ.test.oTestLeftmostFirst);
 
 			var logger = new YAHOO.tool.TestLogger("testLogger_SFJ");
 			logger.hideCategory("info");
